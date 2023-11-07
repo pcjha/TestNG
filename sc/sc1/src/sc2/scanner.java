56ff341a6399cb704f816c1f311b37821a25bda6
pcjha <38557830+pcjha@users.noreply.github.com>
2023-11-08 00:15:41 +0530

    Add files via upload

---
+++ sc/sc1/src/sc2/scanner.java
@@ -0,0 +1,26 @@
+package sc2;
+
+import java.util.Scanner;
+
+public class scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enteryour name: ");
+		
+		String name = input.nextLine();
+		
+		System.out.println("You enter your name as : " +name);
+		
+		System.out.println("Please enteryour age: ");
+		
+		String age = input.nextLine();
+				
+				System.out.println("You enter your age as : " +age);
+				input.close();
+	}
+
+}
